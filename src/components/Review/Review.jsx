3f383bd540a31c6044fec7c26c7ff7a559fcaf00
Matt Lissick <mattlissick@gmail.com>
2019-08-24 12:24:04 -0500

    decided to remove the map method for now to simplify

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -7,37 +7,38 @@ class Review extends Component {
 
     render() {
 
-        let feelingRating = this.props.reduxStore.feelingReducer.map((feeling) => {
-            return (<p>{feeling}</p>)
-        })
+        // let feelingRating = this.props.reduxStore.feelingReducer.map((feeling) => {
+        //     return (<p>{feeling}</p>)
+        // })
 
-        let understandingRating = this.props.reduxStore.understandingReducer.map((understanding) => {
-            return (<p>{understanding}</p>)
-        })
+
+        // let understandingRating = this.props.reduxStore.understandingReducer.map((understanding) => {
+        //     return (<p>{understanding}</p>)
+        // })
        
-        let supportRating = this.props.reduxStore.supportReducer.map((support) => {
-            return (<p>{support}</p>)
-        })
+        // let supportRating = this.props.reduxStore.supportReducer.map((support) => {
+        //     return (<p>{support}</p>)
+        // })
 
-        let comments = this.props.reduxStore.commentsReducer.map((comment) => {
-            return (<p>{comment}</p>)
-        })
+        // let comments = this.props.reduxStore.commentsReducer.map((comment) => {
+        //     return (<p>{comment}</p>)
+        // })
 
         return (
             <div>
                 <h1>Review Your Feedback:</h1>
                 <br/>
                 
-                <h3>feeling score: {feelingRating}</h3>
+                <h3>feeling score: {this.props.reduxStore.feelingReducer[0]}</h3>
                 <br/>
 
-                <h3>understanding score: {understandingRating}</h3>
+                <h3>understanding score: {this.props.reduxStore.understandingReducer[0]}</h3>
                 <br/>
 
-                <h3>support score: {supportRating}</h3>
+                <h3>support score: {this.props.reduxStore.supportReducer[0]}</h3>
                 <br />
 
-                <h3>comments: {comments}</h3>
+                <h3>comments: {this.props.reduxStore.commentsReducer[0]}</h3>
                 <br />
 
                 <button>Submit</button>
