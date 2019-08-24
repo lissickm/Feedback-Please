be2b67de33222f3d4963f1128e61660506a6aff3
Matt Lissick <mattlissick@gmail.com>
2019-08-23 20:07:34 -0500

    set up the content on the feeling and understanding pages

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -8,7 +8,20 @@ class Feeling extends Component {
 
         return(
             <div>
-                <h1>IN FEELING</h1>
+                <h1>How are you feeling today?</h1>
+            
+            <br />
+
+            <select name="ratings">
+                <option value="option1">1 (very stressed)</option>
+                <option value="option2">2</option>
+                <option value="option3">3 (doing ok)</option>
+                <option value="option4" selected>4</option>
+                <option value="option5">5 (feeling great!)</option>
+            </select>
+
+            <button>NEXT</button>
+
             </div >
         )
 
--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -8,7 +8,20 @@ class Understanding extends Component {
 
         return (
             <div>
-                <h1>IN UNDERSTANDING</h1>
+                <h1>How well are you understanding the content?</h1>
+
+                <br />
+
+                <select name="ratings">
+                    <option value="option1">1 (I'm totally lost.)</option>
+                    <option value="option2">2</option>
+                    <option value="option3">3 (doing ok)</option>
+                    <option value="option4" selected>4</option>
+                    <option value="option5">5 (I've got this!)</option>
+                </select>
+
+                <button>NEXT</button>
+
             </div >
         )
 
